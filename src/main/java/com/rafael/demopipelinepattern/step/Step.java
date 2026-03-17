64494a087bc5fe86e91f780b3b9e2e7120654e56
rafaelalmeida <rafael.almeida@ab-inbev.com>
2026-03-17 19:05:24 -0300

    steps dependencies

--- src/main/java/com/rafael/demopipelinepattern/step/Step.java
+++ src/main/java/com/rafael/demopipelinepattern/step/Step.java
@@ -3,10 +3,16 @@ package com.rafael.demopipelinepattern.step;
 import com.rafael.demopipelinepattern.models.Context;
 import com.rafael.demopipelinepattern.models.StepDefinition;
 
+import java.util.Collections;
+import java.util.List;
+
 public interface Step {
     void run(final Context context);
     default boolean shouldRun(final Context context){
         return true;
     }
     StepDefinition definition();
+    default List<StepDefinition> dependsOn() {
+        return Collections.emptyList();
+    }
 }
